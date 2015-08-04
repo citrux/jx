f9313ed5174ac9315c5732b0836e94a6714ae72e
Vova Abdrakhmanov <369565@gmail.com>
2015-08-04 18:29:35 +0300

    start with iterations

---
+++ iterations.cpp
@@ -0,0 +1,19 @@
+#include <complex>
+
+// число итераций для заданного z, при котором
+// значение не вываливается за пределы круга
+// внутри которого имеет смысл искать устойчивые точки
+int iterations(std::complex<double> z, std::complex<double> c, int n_max) {
+
+    auto r = (1 + sqrt(1 + 4 * abs(c))) / 2;
+
+    for (int n = 0; n < n_max; ++n)
+    {
+        z = z * z + c;
+
+        if (abs(z) > r)
+            return n;
+    }
+
+    return n_max;
+}
