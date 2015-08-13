0252edbe4a33863ab594c446577e0553aa4b26ee
Vova Abdrakhmanov <369565@gmail.com>
2015-08-13 15:40:06 +0300

    [add] show constant on mandelbrot set

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,75 @@
 #include <iostream>
 #include <fstream>
 
+class Window {
+    public:
+        Window(std::string title, int width, int height);
+        int width, height;
 
-GLuint p;
-const int window_width = 800;
-const int window_height = 600;
-int max_size = window_height;
-int origin_x = window_width / 2;
-int origin_y = window_height / 2;
-int n_max = 200;
+        void setSpecialInput(void(*)(int key, int x, int y));
+        void setInput(void(*)(unsigned char key, int x, int y));
+        void setRender(void(*)());
+        void setReshape(void(*)(int w, int h));
 
-float scalex = 5. / max_size * window_width;
-float scaley = 5. / max_size * window_height;
+};
 
-float ox = 0.5;
-float oy = 0.5;
+Window::Window(std::string title, int width, int height) :
+    width(width), height(height)
+{
+    glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
+    glutInitWindowSize(width, height);   // Set the window's initial width & height
+    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+    glutCreateWindow(title.c_str()); // Create a window with the given title
+    glewInit();
+}
 
-float cx = -0.24;
-float cy = 0.14;
+void Window::setSpecialInput(void(*f)(int key, int x, int y)) {glutSpecialFunc(f);};
+void Window::setInput(void(*f)(unsigned char key, int x, int y)) {glutKeyboardFunc(f);};
+void Window::setRender(void(*f)()) {glutDisplayFunc(f);};
+void Window::setReshape(void(*f)(int w, int h)) {glutReshapeFunc(f);};
+
+class vec2 {
+    public:
+    vec2(float x, float y) : x(x), y(y) {};
+    float x, y;
+};
+
+vec2 operator+(const vec2 & lhs, const vec2 & rhs) { return vec2(lhs.x + rhs.x, lhs.y + rhs.y); };
+vec2 operator-(const vec2 & lhs, const vec2 & rhs) { return vec2(lhs.x - rhs.x, lhs.y - rhs.y); };
+vec2 operator*(const vec2 & lhs, float rhs) { return vec2(lhs.x * rhs, lhs.y * rhs); };
+vec2 operator/(const vec2 & lhs, float rhs) { return vec2(lhs.x / rhs, lhs.y / rhs); };
+
+struct Fractal {
+    float scale;
+    vec2 origin;
+
+    int iterations;
+
+    GLuint program;
+};
+
+Window* w;
+Fractal julia = {3.0, {0.5, 0.5}, 100, 0};
+Fractal mbrot = {3.0, {0.5, 0.5}, 100, 0};
+bool show_julia = false;
+float scale_factor = 1.2;
+vec2 center(0.5, 0.5);
+vec2 c(-0.74, 0.14);
+Fractal* current = &julia;
 
 void special_input(int key, int x, int y) {
     switch(key) {
         case GLUT_KEY_UP:
-            oy -= 0.05;
+            current->origin.y -= 0.05;
             break;
         case GLUT_KEY_DOWN:
-            oy += 0.05;
+            current->origin.y += 0.05;
             break;
         case GLUT_KEY_LEFT:
-            ox += 0.05;
+            current->origin.x += 0.05;
             break;
         case GLUT_KEY_RIGHT:
-            ox -= 0.05;
+            current->origin.x -= 0.05;
             break;
     }
     glutPostRedisplay();
@@ -42,61 +80,65 @@ void special_input(int key, int x, int y) {
 void input(unsigned char key, int x, int y) {
     switch(key) {
         case 'w':
-            cy += 0.001;
+            c.y += 0.001;
             break;
         case 's':
-            cy -= 0.001;
+            c.y -= 0.001;
             break;
         case 'a':
-            cx -= 0.001;
+            c.x -= 0.001;
             break;
         case 'd':
-            cx += 0.001;
+            c.x += 0.001;
             break;
         case '-':
-            n_max -= 10;
+            current->iterations -= 10;
             break;
         case '=':
-            n_max += 10;
+            current->iterations += 10;
             break;
         case '_':
-            n_max -= int(0.2 * n_max);
+            current->iterations -= int(0.2 * current->iterations);
             break;
         case '+':
-            n_max += int(0.2*n_max);
+            current->iterations += int(0.2*current->iterations);
             break;
         case '0':
-            scalex /= 1.2;
-            scaley /= 1.2;
-            ox = 0.5 + (ox - 0.5) * 1.2;
-            oy = 0.5 + (oy - 0.5) * 1.2;
+            current->scale /= scale_factor;
+            current->origin = center + (current->origin - center) * scale_factor;
             break;
         case '9':
-            scalex *= 1.2;
-            scaley *= 1.2;
-            ox = 0.5 + (ox - 0.5) / 1.2;
-            oy = 0.5 + (oy - 0.5) / 1.2;
+            current->scale *= scale_factor;
+            current->origin = center + (current->origin - center) / scale_factor;
+            break;
+        case 'm':
+            show_julia = !show_julia;
             break;
         case 'q':
             exit(0);
             break;
     }
-    std::cout << cx << ", " << cy << std::endl;
+    std::cout << c.x << ", " << c.y << std::endl;
     glutPostRedisplay();
 }
 
-/* Handler for window-repaint event. Call back when the window first appears and
-   whenever the window needs to be re-painted. */
-void display() {
+void render() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
     glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
 
-    glUseProgram(p);
+    if (show_julia)
+        current = &julia;
+    else
+        current = &mbrot;
 
-    glUniform2f(glGetUniformLocation(p, "c"), cx, cy);
-    glUniform2f(glGetUniformLocation(p, "scale"), scalex, scaley);
-    glUniform2f(glGetUniformLocation(p, "o"), ox, oy);
-    glUniform1i(glGetUniformLocation(p, "iter"), n_max);
+    glUseProgram(current->program);
+
+    auto max_size = std::max(w->height, w->width);
+    auto scale = vec2(current->scale / max_size * w->width, current->scale / max_size * w->height);
+    glUniform2f(glGetUniformLocation(current->program, "c"), c.x, c.y);
+    glUniform2f(glGetUniformLocation(current->program, "scale"), scale.x, scale.y);
+    glUniform2f(glGetUniformLocation(current->program, "o"), current->origin.x, current->origin.y);
+    glUniform1i(glGetUniformLocation(current->program, "iter"), current->iterations);
 
     glBegin(GL_POLYGON);
     glTexCoord2f(1, 0);
@@ -113,51 +155,62 @@ void display() {
     glutSwapBuffers();
 }
 
-void setShaders() {
-    GLuint v;
+GLuint compileShader(const char* fileName, GLenum shaderType) {
+    GLuint handler;
 
     // get a shader handler
-    v = glCreateShader(GL_FRAGMENT_SHADER);
+    handler = glCreateShader(shaderType);
     // read the shader source from a file
-    std::ifstream is("src/julia.glsl", std::ios::in|std::ios::binary|std::ios::ate);
+    std::ifstream is(fileName, std::ios::in|std::ios::binary|std::ios::ate);
 	if (!is.is_open()) {
-		std::cerr << "Unable to open file " << "src/julia.glsl" << std::endl;
+		std::cerr << "Unable to open file " << fileName << std::endl;
 		exit(1);
 	}
+
 	long size = is.tellg();
-	char *vs = new char[size+1];
+
+    auto buffer = new char[size+1];
 	is.seekg(0, std::ios::beg);
-	is.read (vs, size);
+	is.read (buffer, size);
 	is.close();
-	vs[size] = 0;
+	buffer[size] = 0;
 
     // conversions to fit the next function
-    const char *vv = vs;
+    const char* const_buffer = buffer;
     // pass the source text to GL
-    glShaderSource(v, 1, &vv,NULL);
+    glShaderSource(handler, 1, &const_buffer, 0);
     // free the memory from the source text
-    delete[] vs;
+    delete[] buffer;
     // finally compile the shader
-    glCompileShader(v);
+    glCompileShader(handler);
+    return handler;
+}
 
-    p = glCreateProgram();
-    glAttachShader(p, v);
-    glLinkProgram(p);
+void addShader(GLuint program, GLuint shader) {
+    glAttachShader(program, shader);
+    glLinkProgram(program);
 };
+
+void reshape(int width, int height) {w->width=width; w->height=height;};
 
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
     glutInit(&argc, argv);                 // Initialize GLUT
-    glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
-    glutInitWindowSize(window_width, window_height);   // Set the window's initial width & height
-    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-    glutCreateWindow("Julia set explorer"); // Create a window with the given title
-    glutSpecialFunc(special_input);
-    glutKeyboardFunc(input);
-    glutDisplayFunc(display); // Register display callback handler for window re-paint
+    w = new Window("Julia set", 800, 600);
+    w->setInput(input);
+    w->setSpecialInput(special_input);
+    w->setRender(render);
+    w->setReshape(reshape);
 
-    glewInit();
-	setShaders();
+    auto shader = compileShader("./src/julia.glsl", GL_FRAGMENT_SHADER);
+    auto p = glCreateProgram();
+    addShader(p, shader);
+    julia.program = p;
+
+    shader = compileShader("./src/mbrot.glsl", GL_FRAGMENT_SHADER);
+    p = glCreateProgram();
+    addShader(p, shader);
+    mbrot.program = p;
 
     glutMainLoop();           // Enter the infinitely event-processing loop
     return 0;
---
+++ src/mbrot.glsl
@@ -0,0 +1,33 @@
+uniform vec2 c;
+uniform vec2 o;
+uniform vec2 scale;
+uniform int iter;
+
+void main() {
+    vec2 z;
+	z.x = scale.x * (gl_TexCoord[0].x - o.x);
+    z.y = scale.y * (gl_TexCoord[0].y - o.y);
+
+    float eps = 0.005 * scale.x;
+    if (pow(z.x - c.x, 2) +
+        pow(z.y - c.y, 2) < eps * eps) {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        return;
+    }
+
+    vec2 w = z;
+    int i;
+    float r = 0.5 + sqrt(0.25 + sqrt(z.x * z.x + z.y * z.y));
+    for(i=0; i<iter; i++) {
+        float x = w.x * w.x - w.y * w.y + z.x;
+        float y = w.x * w.y + w.x * w.y + z.y;
+
+        if((x * x + y * y) > r * r) break;
+        w.x = x;
+        w.y = y;
+    }
+
+    float a = float(i) / float(iter);
+    gl_FragColor = vec4(0.2 + 0.8 * (0.5 - 0.5 * cos(9.42 * a)),  0.6 + 0.4 * (0.5 - 0.5 * cos(9.42 * a)),
+    1.0 - 0.2 * (a - a * a), 1.0);
+}
